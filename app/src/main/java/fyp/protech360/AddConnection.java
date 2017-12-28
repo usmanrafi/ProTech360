42482d639353962bd37742935bcf7a812b89b78a
Aliyan <syedsajjasali@gmail.com>
2017-12-29 00:31:33 +0500

    Add Connections added

--- app/src/main/java/fyp/protech360/AddConnection.java
+++ app/src/main/java/fyp/protech360/AddConnection.java
@@ -1,12 +1,16 @@
 package fyp.protech360;
 
 import android.app.Fragment;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 /**
@@ -15,12 +19,42 @@ import android.widget.ListView;
 
 public class AddConnection extends Fragment {
     View myView;
+    LinearLayout l1, l2;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.connected_devices,container,false);
+        myView = inflater.inflate(R.layout.addconnection,container,false);
         ((Homepage) getActivity()).setActionBarTitle("Add a Device");
+
+        l1 = (LinearLayout) myView.findViewById(R.id.smartphoneList);
+        l2 = (LinearLayout) myView.findViewById(R.id.wearableList);
+
+        TabLayout tabLayout = (TabLayout) myView.findViewById(R.id.tab);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0){
+                    l1.setVisibility(View.VISIBLE);
+                    l2.setVisibility(View.GONE);
+                }
+                else{
+                    l2.setVisibility(View.VISIBLE);
+                    l1.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
         return myView;
