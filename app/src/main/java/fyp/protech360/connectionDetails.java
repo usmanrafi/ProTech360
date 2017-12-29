99ba390e31ac575a72aa6cf8f9528840b742adc7
Usman Rafi <usman.rafi.96@gmail.com>
2017-12-29 06:40:14 +0500

    Added Directional Polylines

--- app/src/main/java/fyp/protech360/connectionDetails.java
+++ app/src/main/java/fyp/protech360/connectionDetails.java
@@ -3,7 +3,9 @@ package fyp.protech360;
 
 import android.*;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,6 +19,7 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.DirectionsResult;
@@ -32,6 +35,8 @@ public class connectionDetails extends Fragment implements OnMapReadyCallback{
     View myView;
     MapView myMapView;
     GoogleMap myMap;
+
+    FloatingActionButton dir;
 
     public connectionDetails() {
         // Required empty public constructor
@@ -50,6 +55,8 @@ public class connectionDetails extends Fragment implements OnMapReadyCallback{
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dir = myView.findViewById(R.id.getDirections);
+
         myMapView = myView.findViewById(R.id.map);
 
         if (myMapView != null) {
@@ -57,6 +64,8 @@ public class connectionDetails extends Fragment implements OnMapReadyCallback{
             myMapView.onResume();
             myMapView.getMapAsync(this);
         }
+
+
     }
 
     @Override
@@ -80,6 +89,28 @@ public class connectionDetails extends Fragment implements OnMapReadyCallback{
 
         myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(31.5204,74.3587),10));
 
+
+        dir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(31.4820022,74.3025007))
+                .add(new LatLng(31.4834161,74.3063229))
+                .add(new LatLng(31.4867392,74.30468379999999))
+                .add(new LatLng(31.4871349,74.30483529999999))
+                .add(new LatLng(31.5002922,74.3171364))
+                .add(new LatLng(31.502941,74.3262769))
+                .add(new LatLng(31.5057629,74.3348426))
+                .add(new LatLng(31.5101101, 74.3403089))
+                .add(new LatLng(31.5114741,74.3410411))
+                .add(new LatLng(31.5230457,74.3476297))
+                .add(new LatLng(31.52297669999999,74.34786989999999))
+                .add(new LatLng(31.5214012,74.34703619999999))
+                .add(new LatLng(31.5209553,74.3510512))
+                .add(new LatLng(31.5232865,74.3550096))
+                .add(new LatLng(31.520362,74.35872239999999)).color(Color.BLUE));
+            }
+        });
     }
 
 }
