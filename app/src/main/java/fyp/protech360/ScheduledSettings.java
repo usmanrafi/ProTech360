7d9dafa63acd372f4d39f97c39056acee7b007a9
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-05 23:52:37 +0500

    Modified TimeBasedProfiles

--- app/src/main/java/fyp/protech360/ScheduledSettings.java
+++ app/src/main/java/fyp/protech360/ScheduledSettings.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ScheduledSettings extends Fragment {
     View myView;
@@ -15,7 +16,6 @@ public class ScheduledSettings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_scheduled_settings, container, false);
         ((Homepage) getActivity()).setActionBarTitle("Scheduled Settings");
-
 
         return myView;
     }
--- app/src/main/java/fyp/protech360/TimeBasedProfiles.java
+++ app/src/main/java/fyp/protech360/TimeBasedProfiles.java
@@ -1,5 +1,6 @@
 package fyp.protech360;
 
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+
 
 
 public class TimeBasedProfiles extends Fragment{
@@ -15,18 +18,31 @@ public class TimeBasedProfiles extends Fragment{
     View myView;
     Spinner spinner;
 
+    Button submit;
+    AudioManager am;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         myView = inflater.inflate(R.layout.fragment_time_based_profiles, container, false);
-//        ((TrackRoomDetails) getActivity()).setActionBarTitle("TrackRoom");
+        //        ((TrackRoomDetails) getActivity()).setActionBarTitle("TrackRoom");
 
         spinner = myView.findViewById(R.id.profile_spinner);
+        submit = myView.findViewById(R.id.submitTimeBasedProfiles);
 
         if(spinner != null){
             String[] list = getResources().getStringArray(R.array.spinner_profiles);
             spinner.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.layout_spinner,R.id.spinner_txt,list));
         }
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                am.setRingerMode(0);
+            }
+        });
         return myView;
     }
 
