88f9e0cfbccfd136f0441c7deab95a58a202e557
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-06 22:45:20 +0500

    Modified EmergencyDetails.java

--- app/src/main/java/fyp/protech360/EmergencyDetails.java
+++ app/src/main/java/fyp/protech360/EmergencyDetails.java
@@ -1,5 +1,7 @@
 package fyp.protech360;
 
+import android.telephony.SmsManager;
+
 public class EmergencyDetails {
     private String message;
     private String num1;
@@ -18,5 +20,20 @@ public class EmergencyDetails {
     public String[] getNumbers(){
         String[] nums = {this.num1, this.num2, this.num3};
         return nums;
+    }
+
+    public void setMessage(String msg){
+        this.message = msg;
+    }
+
+    public void sendPanicAlert(String latitude, String longitude){
+
+        String Url = "http://www.google.com/maps/place/" + latitude + "," + longitude;
+        String messageToSend = "ALERT!!! I am in trouble, please help me.\n" +
+                "My current location is: " + Url + ".\nPlease hurry up!\n\nSent via ProTech360";
+
+        SmsManager.getDefault().sendTextMessage(num1, null, messageToSend, null, null);
+        SmsManager.getDefault().sendTextMessage(num2, null, messageToSend, null, null);
+        SmsManager.getDefault().sendTextMessage(num3, null, messageToSend, null, null);
     }
 }
