a5594301c49cf088f6ca953624d4986dfedda014
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-06 22:58:20 +0500

    Modified EmergencyDetails.java

--- app/src/main/java/fyp/protech360/EmergencyDetails.java
+++ app/src/main/java/fyp/protech360/EmergencyDetails.java
@@ -8,8 +8,15 @@ public class EmergencyDetails {
     private String num2;
     private String num3;
 
+    EmergencyDetails(){
+        this.message = "ALERT!!! I am in trouble, please help me!";
+        this.num1 = null;
+        this.num2 = null;
+        this.num3 = null;
+    }
+
     EmergencyDetails(String msg, String n1, String n2, String n3){
-        this.message = message;
+        this.message = msg;
         this.num1 = n1;
         this.num2 = n2;
         this.num3 = n3;
@@ -26,14 +33,37 @@ public class EmergencyDetails {
         this.message = msg;
     }
 
+    public boolean validNumbers(){
+        if(this.num1 == null && this.num2 == null && this.num3 == null)
+            return false;
+        else {
+            try {
+                if (this.num1.length() != 13
+                        && this.num2.length() != 13
+                        && this.num3.length() != 13)
+                    return false;
+            }catch (NullPointerException e){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public void sendPanicAlert(String latitude, String longitude){
 
         String Url = "http://www.google.com/maps/place/" + latitude + "," + longitude;
-        String messageToSend = "ALERT!!! I am in trouble, please help me.\n" +
+        String messageToSend = this.message + "\n" +
                 "My current location is: " + Url + ".\nPlease hurry up!\n\nSent via ProTech360";
 
-        SmsManager.getDefault().sendTextMessage(num1, null, messageToSend, null, null);
-        SmsManager.getDefault().sendTextMessage(num2, null, messageToSend, null, null);
-        SmsManager.getDefault().sendTextMessage(num3, null, messageToSend, null, null);
+
+        if(num1 != null)
+            SmsManager.getDefault().sendTextMessage(num1, null, messageToSend, null, null);
+
+        if(num2 != null)
+            SmsManager.getDefault().sendTextMessage(num2, null, messageToSend, null, null);
+
+        if(num3 != null)
+            SmsManager.getDefault().sendTextMessage(num3, null, messageToSend, null, null);
     }
 }
