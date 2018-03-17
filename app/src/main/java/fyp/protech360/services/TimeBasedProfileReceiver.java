0a9ba407732273cc7aa884603b7c006253f8466b
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-17 22:00:19 +0500

    Added TimeBasedProfileReceiver

---
+++ app/src/main/java/fyp/protech360/services/TimeBasedProfileReceiver.java
@@ -0,0 +1,12 @@
+package fyp.protech360.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TimeBasedProfileReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //set scheduled profiles
+    }
+}
