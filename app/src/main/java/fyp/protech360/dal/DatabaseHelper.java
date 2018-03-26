740eb582ba6f2ba69a6001e3e44e17645b26ba1a
Usman Rafi <usman.rafi.96@gmail.com>
2018-03-26 21:52:39 +0500

    Modified DatabaseHelper.java

--- app/src/main/java/fyp/protech360/dal/DatabaseHelper.java
+++ app/src/main/java/fyp/protech360/dal/DatabaseHelper.java
@@ -9,12 +9,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "ProTech360.db";
-    public static final String TABLE_A = "emergency_details";
-    public static final String COL_A1 = "Message";
-    public static final String COL_A2 = "num1";
-    public static final String COL_A3 = "num2";
-    public static final String COL_A4 = "num3";
-    public static final String COL_A5 = "userID";
+    public static final String TABLE_EMERGENCY_DETAILS = "emergency_details";
+    public static final String COL_NUM1 = "Num1";
+    public static final String COL_NUM2 = "Num2";
+    public static final String COL_NUM3 = "Num3";
+    public static final String COL_MESSAGE = "Message";
+    public static final String COL_ID = "ID";
 
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -27,30 +27,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_A +" (" + COL_A1 + " TEXT," + COL_A2 + " TEXT," + COL_A3 + " TEXT," + COL_A4 + " TEXT,"+COL_A5+" LONG, PRIMARY KEY("+COL_A5+"))");
+        db.execSQL("create table " + TABLE_EMERGENCY_DETAILS +" (" + COL_NUM1 + " TEXT," + COL_NUM2 + " TEXT," + COL_NUM3+ " TEXT," + COL_MESSAGE + " TEXT,"+ COL_ID +" INT, PRIMARY KEY("+COL_ID+"))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_A);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_EMERGENCY_DETAILS);
         onCreate(db);
     }
 
-    public boolean insertData(String message,String num1, String num2, String num3, Long userID) {
+    public boolean insertData(String message,String num1, String num2, String num3) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_A1,message);
-        contentValues.put(COL_A2,num1);
-        contentValues.put(COL_A3,num2);
-        contentValues.put(COL_A4,num3);
-        contentValues.put(COL_A5,userID);
-        long result = db.insert(TABLE_A,null,contentValues);
+        contentValues.put(COL_ID,1);
+        contentValues.put(COL_MESSAGE,message);
+        contentValues.put(COL_NUM1,num1);
+        contentValues.put(COL_NUM2,num2);
+        contentValues.put(COL_NUM3,num3);
+        long result = db.insert(TABLE_EMERGENCY_DETAILS,null,contentValues);
         return (result != -1);
     }
 
     public Cursor getEmergencyData(String userID) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.query(true,TABLE_A,new String[]{COL_A1,COL_A2,COL_A3,COL_A4,COL_A5},COL_A5 + " = ?",new String[]{userID},null,null,null,null);
+        Cursor res = db.query(TABLE_EMERGENCY_DETAILS,new String[]{COL_MESSAGE,COL_NUM1,COL_NUM2,COL_NUM3,COL_ID},null,null,null,null,null);
         return res;
     }
 
