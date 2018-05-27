c60ce74a6a77146339c391420d6e7e72875ca364
Aliyan <syedsajjasali@gmail.com>
2018-05-27 11:12:28 +0500

    Added Meeting Details

--- app/src/main/java/fyp/protech360/ui/MeetingDetails.java
+++ app/src/main/java/fyp/protech360/ui/MeetingDetails.java
@@ -2,9 +2,27 @@ package fyp.protech360.ui;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import fyp.protech360.R;
+import fyp.protech360.classes.Meeting;
+import fyp.protech360.classes.User;
+import fyp.protech360.utils.ConnectionAdapter;
 
 /**
  * Created by Aliyan on 5/27/2018.
@@ -14,6 +32,12 @@ public class MeetingDetails extends Fragment {
 
     View myView;
     String meetingID;
+    Meeting meeting;
+    TextView date, time;
+    Button location, reminder, update, cancel;
+    ListView participantsList;
+    ConnectionAdapter participantsAdapter;
+    ArrayList<User> participants = new ArrayList<>();
 
 
     @Override
@@ -23,10 +47,70 @@ public class MeetingDetails extends Fragment {
 
         meetingID = getArguments().getString("Meeting");
 
-        ((Homepage) getActivity()).setActionBarTitle("Meetings");
+        date = myView.findViewById(R.id.meeting_date);
+        time = myView.findViewById(R.id.meeting_time);
+        location = myView.findViewById(R.id.meeting_location);
+        reminder = myView.findViewById(R.id.meeting_reminder);
+        update = myView.findViewById(R.id.meeting_update);
+        cancel = myView.findViewById(R.id.meeting_cancel);
+        participantsList = myView.findViewById(R.id.meeting_participants);
+
+        participantsList.setClickable(true);
+
+        meetingStatus();
+        participantsAdapter = new ConnectionAdapter(getActivity(), R.layout.connectionslist_row, participants);
+        participantsList.setAdapter(participantsAdapter);
+
 
 
         return myView;
+    }
+
+
+    public void meetingStatus()
+    {
+        DatabaseReference meetingRef = FirebaseDatabase.getInstance().getReference("Meetings").child(meetingID);
+        meetingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                meeting = dataSnapshot.getValue(Meeting.class);
+        //        participants = meeting.getParticipants();
+                addViews();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addViews() {
+
+        ((Homepage) getActivity()).setActionBarTitle( meeting.getName());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(meeting.getTime());
+        String dayString = "", monthString = "", yearString = "", hourString = "", minuteString = "";
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int year = calendar.get(Calendar.YEAR);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        dayString = String.valueOf(day);
+        monthString = String.valueOf(month);
+        yearString = String.valueOf(year);
+        hourString = String.valueOf(hour);
+        minuteString = String.valueOf(minute);
+
+        if(day < 10) dayString = "0"+ dayString;
+        if(month < 10) monthString = "0"+ monthString;
+        if(hour < 10) hourString = "0"+ hourString;
+        if(minute < 10) minuteString = "0"+ minuteString;
+
+        date.setText(dayString + "/" + monthString + "/" + yearString);
+        time.setText(hourString + ":" + minuteString);
+
     }
 
 
